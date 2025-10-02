38702f7e879a254ed25c6a5a9f04c6f729e8750a
Marcelo <60022427+cavatti@users.noreply.github.com>
2025-10-02 21:47:37 +0200

    comit

--- test.sh
+++ test.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
+
+# Pega o primeiro parâmetro passado, ou usa "mundo" como padrão
 NAME=${1:-mundo}
 
-curl -XPOST "http://localhost:8080/2015-03-31/functions/function/invocations" \
+# Envia evento JSON completo para o Lambda container local
+curl -s -XPOST "http://localhost:8080/2015-03-31/functions/function/invocations" \
 -H "Content-Type: application/json" \
 -d "{
-  \"httpMethod\": \"GET\",
-  \"path\": \"/hello\",
+  \"version\": \"2.0\",
+  \"routeKey\": \"GET /hello\",
+  \"rawPath\": \"/hello\",
+  \"rawQueryString\": \"name=$NAME\",
+  \"headers\": {},
   \"queryStringParameters\": {\"name\": \"$NAME\"},
-  \"headers\": {}
-}"
+  \"requestContext\": {
+    \"http\": {
+      \"method\": \"GET\",
+      \"path\": \"/hello\",
+      \"sourceIp\": \"127.0.0.1\",
+      \"userAgent\": \"curl/8.2.1\"
+    }
+  },
+  \"body\": null,
+  \"isBase64Encoded\": false
+}" | jq -r '.body | fromjson'
+
+# Quebra de linha no terminal
 echo
